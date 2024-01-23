05333fd2bb9242ce762dd0f27abaa9f2deb5c728
Cameron Riddell <cameron@dutc.io>
2024-01-23 13:49:54 -0800

    simplify reproduce scripts
    environment creation/management should happen outside of reproduce.sh
    Additionally round tripping notebooks to execute can swapped for
    jupyter-execute.

--- reproduce_jupyter.sh
+++ reproduce_jupyter.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-# install the versioned required packages
-python -m venv venv
-source ./venv/bin/activate
-echo "Installing requirements.txt (Python packages)"
-pip install -r binder/requirements_jupyter.txt --quiet
 
-# navigate to code/ and execute the python file to create figures
-# pip list
-#cd ./code/python
 jupyter notebook ./code/python/Main_KS.ipynb
-
-# Cleanup
-#cd ../..
-deactivate
-#rm -rf venv
